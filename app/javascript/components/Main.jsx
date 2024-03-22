f7525be96190f64b408b676ade291cc301389e1c
linuxpro3000 <83082486+aleksandar-code@users.noreply.github.com>
2024-03-22 09:06:56 +0100

    Modify drop down function

--- app/javascript/components/Main.jsx
+++ app/javascript/components/Main.jsx
@@ -2,47 +2,52 @@ import React, { useRef, useState } from "react";
 import { Link, useLocation } from 'react-router-dom';
 
 export default () => {
-  const level1 = useRef(null);
-  const startBtn = useRef(null);
-  const dropDownDom = useRef(null);
 
-  function changeGameStatus() {
-    if (startBtn) {
-      if (startBtn.current.classList[1] == "enabled") {
-        startBtn.current.classList.replace("enabled", "disabled");
-        level1.current.src = "images/Where-is-waldo-1.png";
+    const level1 = useRef(null);
+    const startBtn = useRef(null);
+    const dropDownDom = useRef(null);
+
+    function changeGameStatus() {
+      if (startBtn) {
+        if (startBtn.current.classList[1] == "enabled") {
+          startBtn.current.classList.replace("enabled", "disabled");
+          level1.current.src = "images/Where-is-waldo-1.png";
+          level1.current.style.display = "initial";
+          console.log("i have been run");
+          window.addEventListener("click", (e) => {
+            if (dropDownDom.current != null) {
+              if (dropDownDom.current.classList[1] == "enabled" && e.target.localName !== "img") {
+                console.log(e.target.localName);
+                dropDownDom.current.classList.replace("enabled", "disabled");
+              }
+            }
+          });
+        }
       }
     }
-  }
 
-  function dropDown(e) {
-    if (dropDownDom) {
-      const xPos = e.clientX;
-      const yPos = e.clientY;
-      if (dropDownDom.current.classList[1] == "disabled"){
-      dropDownDom.current.classList.replace("disabled", "enabled");
-      console.log(xPos, yPos);
-      dropDownDom.current.style.top = yPos + "px" ;
-      dropDownDom.current.style.left = xPos + "px";
-      } 
-    else {
-      console.log(xPos, yPos)
-      dropDownDom.current.style.top = yPos + "px" ;
-      dropDownDom.current.style.left = xPos + "px";
-      console.log(e.target.localName);
-      }
-    }
-  }
+    function dropDown(e) {
+      if (dropDownDom) {
+        const rect = level1.current.getBoundingClientRect(); 
+        const xPos = e.clientX - rect.left;
+        const yPos = e.clientY - rect.top;
 
-  window.addEventListener("click", (e) => {
-    if (dropDownDom) {
-      if (dropDownDom.current.classList[1] == "enabled" && e.target.localName !== "img") {
+        if (dropDownDom.current.classList[1] == "disabled"){
+          dropDownDom.current.classList.replace("disabled", "enabled")
+          console.log('Cursor position: ' + xPos + ',' + yPos);
+          console.log(e.target.localName);
+          dropDownDom.current.style.left = xPos + "px";
+          dropDownDom.current.style.top = yPos + "px";
+        } 
+      else {
+        dropDownDom.current.style.left = xPos + "px";
+        dropDownDom.current.style.top = yPos + "px";
+        console.log('Cursor position: ' + xPos + ',' + yPos);
         console.log(e.target.localName);
-        dropDownDom.current.classList.replace("enabled", "disabled");
+        }
       }
     }
-  });
-
+  
   return (
     <main>
     {useLocation().pathname == '/' ?
@@ -54,7 +59,7 @@ export default () => {
           <li>Wizard</li>
           <li>Odlaw</li>
         </div>
-        <img ref={level1} src={" "} height="720" width="1280" alt="" onClick={dropDown} draggable="false" style={{ userSelect: "none" }}/>
+        <img ref={level1} src={" "} height="720" width="1280" alt="" onClick={dropDown} draggable="false" style={{ userSelect: "none", display: "none" }}/>
       </div>
 
     </> :
