241e41b522dc7c5a0d15bf9c9d03b7371f9fd282
linuxpro3000 <83082486+aleksandar-code@users.noreply.github.com>
2024-03-24 09:40:45 +0100

    Add characters and migrate

---
+++ db/migrate/20240324084008_create_characters.rb
@@ -0,0 +1,11 @@
+class CreateCharacters < ActiveRecord::Migration[7.1]
+  def change
+    create_table :characters do |t|
+      t.string :name
+      t.boolean :found
+      t.integer :pixel_location
+
+      t.timestamps
+    end
+  end
+end
