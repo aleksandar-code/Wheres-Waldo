0d139ae034c170575a7fa4ec7964898d3986a65c
linuxpro3000 <83082486+aleksandar-code@users.noreply.github.com>
2024-03-25 17:48:06 +0100

    Fetch data from backend

--- app/controllers/api/v1/levels_controller.rb
+++ app/controllers/api/v1/levels_controller.rb
@@ -1,4 +1,7 @@
 class Api::V1::LevelsController < ApplicationController
   def index
+    level = Level.all.order(created_at: :desc)
+    characters = Level.all.order(created_at: :desc).first.characters
+    render json: [level, characters]
   end
 end
--- app/javascript/components/Main.jsx
+++ app/javascript/components/Main.jsx
@@ -1,48 +1,61 @@
-import React, { useRef, useState } from "react";
-import { Link, useLocation } from 'react-router-dom';
+import React, { useRef, useState, useEffect } from "react";
+import { Link, useLocation, useNavigate} from 'react-router-dom';
 
 export default () => {
-
-    const level1 = useRef(null);
-    const startBtn = useRef(null);
-    const dropDownDom = useRef(null);
-
-    function changeGameStatus() {
-      if (startBtn) {
-        if (startBtn.current.classList[1] == "enabled") {
-          startBtn.current.classList.replace("enabled", "disabled");
-          level1.current.src = "images/Where-is-waldo-1.png";
-          level1.current.style.display = "initial";
-          console.log("i have been run");
-          window.addEventListener("click", (e) => {
-            if (dropDownDom.current != null) {
-              if (dropDownDom.current.classList[1] == "enabled" && e.target.localName !== "img") {
-                console.log(e.target.localName);
-                dropDownDom.current.classList.replace("enabled", "disabled");
-              }
-            }
-          });
+  const navigate = useNavigate();
+  const [levels, setLevels] = useState([]);
+  useEffect(() => {
+    const url = "/api/v1/levels/index";
+    fetch(url)
+      .then((res) => {
+        if (res.ok) {
+          return res.json();
         }
+        throw new Error("Network response was not ok.");
+      })
+      .then((res) => setLevels(res))
+      .catch(() => navigate("/"));
+  }, []);
+  
+  const level1 = useRef(null);
+  const startBtn = useRef(null);
+  const dropDownDom = useRef(null);
+
+  function changeGameStatus() {
+    if (startBtn) {
+      if (startBtn.current.classList[1] == "enabled") {
+        startBtn.current.classList.replace("enabled", "disabled");
+        level1.current.src = levels[0][0].image;
+        level1.current.style.display = "initial";
+        window.addEventListener("click", (e) => {
+          if (dropDownDom.current != null) {
+            if (dropDownDom.current.classList[1] == "enabled" && e.target.localName !== "img") {
+              console.log(e.target.localName);
+              dropDownDom.current.classList.replace("enabled", "disabled");
+            }
+          }
+        });
       }
     }
+  }
 
-    function dropDown(e) {
-      if (dropDownDom) {
-        const rect = level1.current.getBoundingClientRect(); 
-        const xPos = e.clientX - rect.left;
-        const yPos = e.clientY - rect.top;
+  function dropDown(e) {
+    if (dropDownDom) {
+      const rect = level1.current.getBoundingClientRect(); 
+      const xPos = e.clientX - rect.left;
+      const yPos = e.clientY - rect.top;
 
-        if (dropDownDom.current.classList[1] == "disabled"){
-          dropDownDom.current.classList.replace("disabled", "enabled")
-          dropDownDom.current.style.left = xPos + "px";
-          dropDownDom.current.style.top = yPos + "px";
-        } 
-      else {
+      if (dropDownDom.current.classList[1] == "disabled"){
+        dropDownDom.current.classList.replace("disabled", "enabled")
         dropDownDom.current.style.left = xPos + "px";
         dropDownDom.current.style.top = yPos + "px";
-        }
+      } 
+    else {
+      dropDownDom.current.style.left = xPos + "px";
+      dropDownDom.current.style.top = yPos + "px";
       }
     }
+  }
   
   return (
     <main>
@@ -51,13 +64,17 @@ export default () => {
       <button ref={startBtn} className="start-btn enabled" onClick={changeGameStatus}>PRESS START</button>
       <div className="image">
         <div ref={dropDownDom} className="drop-down disabled" draggable="false" style={{ userSelect: "none" }}>
-          <li>Waldo</li>
-          <li>Wizard</li>
-          <li>Odlaw</li>
+          {levels.length > 1 ?
+          <>
+            {levels[1].map((character) => (
+              <li key={character.id}>{character.name}</li>
+            ))}
+          </>
+        : "Loading"}
+          
         </div>
         <img ref={level1} src={" "} height="720" width="1280" alt="" onClick={dropDown} draggable="false" style={{ userSelect: "none", display: "none" }}/>
       </div>
-
     </> :
     <>
       <div className="leaderboard">
