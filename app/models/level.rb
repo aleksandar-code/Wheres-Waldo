f1384f33768494a7ef1a48fa1c4132da931e5c15
linuxpro3000 <83082486+aleksandar-code@users.noreply.github.com>
2024-04-03 12:03:14 +0200

    Change filter_characters variable names

--- app/models/level.rb
+++ app/models/level.rb
@@ -2,11 +2,11 @@ class Level < ApplicationRecord
   has_many :characters
 
   def self.filter_characters
-    characters = []
-    chars_array = Level.all.order(created_at: :desc).first.characters
-    chars_array.each do |character|
-      characters.push({name: character.name, id: character.id, found: character.found})
+    characters_array = []
+    array = Level.all.order(created_at: :desc).first.characters
+    array.each do |character|
+      characters_array.push({name: character.name, id: character.id, found: character.found})
     end
-    characters
+    characters_array
   end
 end
